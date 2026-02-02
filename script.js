73e5ed624b9adf9ef6e356e3ee14e16f0646aa78
Ashish Kunthe <160906505+ashishkunthe@users.noreply.github.com>
2026-02-02 22:34:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function daysOfYear(year) {
+	function isLeapyear(n){
+		if(n%400===0) return true
+		if(n%100===0) return false
+		if(n%4===0) return true
+		return false
+	}
+	if(isLeapyear(year)){
+		console.log(366)
+	}else{
+		console.log(365)
+	}
+}
